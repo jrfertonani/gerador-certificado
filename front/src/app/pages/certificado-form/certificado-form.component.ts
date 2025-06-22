cf2876b03bd05f72ac18dff965c2ab58ba44c52e
D M I <ademirjuniorfertonani@gmail.com>
2025-06-22 11:22:16 -0300

    ajustes pages certificados

--- front/src/app/pages/certificado-form/certificado-form.component.ts
+++ front/src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { PrimaryButtonComponent } from "../../_components/primary-button/primary-button.component";
-import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
 import { FormsModule, NgModel } from '@angular/forms';
+import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent, FormsModule],
+  imports: [
+    PrimaryButtonComponent,
+    SecondaryButtonComponent,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './certificado-form.component.html',
   styleUrl: './certificado-form.component.css'
 })
@@ -13,11 +20,10 @@ export class CertificadoFormComponent {
 
   nome: string = '';
   atividade: string = '';
-  atividades: string [] = ['Angular','Java', 'sql'];
+  atividades: string[] = ['Angular', 'React', 'Vue'];
 
-
-campoInvalido(control: NgModel){
-  return control.invalid && control.touched
-}
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched;
+  }
 
 }
