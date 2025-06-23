a83af78c844acc483c9819e91ef3fc7963ed841b
D M I <ademirjuniorfertonani@gmail.com>
2025-06-23 20:11:39 -0300

    interface Atividades

--- front/src/app/pages/certificado-form/certificado-form.component.ts
+++ front/src/app/pages/certificado-form/certificado-form.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+import { Certificado } from '../../interfaces/certificado';
 
 
 @Component({
@@ -18,15 +19,35 @@ import { SecondaryButtonComponent } from '../../_components/secondary-button/sec
 })
 export class CertificadoFormComponent {
 
-  nome: string = '';
+
+  certificado: Certificado = {
+    atividades: [],
+    nome: ''
+  };
+
   atividade: string = '';
-  atividades: string[] = ['Angular', 'React', 'Vue'];
 
   campoInvalido(control: NgModel) {
     return control.invalid && control.touched;
   }
 
-  adicionarAtividade(){
-    this.atividades.push(this.atividade);
+  formValido(){
+    return this.certificado.atividades.length > 0 && this.certificado.nome.length > 0;
   }
+
+  adicionarAtividade(){
+    this.certificado.atividades.push(this.atividade);
+    this.atividade = '';
+  }
+
+  excluirAtividade(index: number){
+    this.certificado.atividades.splice(index, 1);
+  }
+
+  submit(){
+    if(!this.formValido()){
+
+    }
+  }
+
 }
